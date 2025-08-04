b775d429ae8ba9ef7e925a5391a080704801dfb8
Bille PavanKalyan <pavankalyanbille@gmail.com>
2025-08-04 15:00:26 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-
-
 import "./global.css";
 import Form from "./Form";
 import TodoList from "./TodoList";
@@ -17,53 +15,56 @@ const App = () => {
   let handleChange = (e) => {
     let { name, value } = e.target;
     setState({ ...state, [name]: value });
-  };  
+  };
 
-  let handleSubmit = (e)=>{
-    e.preventDefault() 
+  let handleSubmit = (e) => {
+    e.preventDefault();
     let newItem = {
-      id : state.id , 
-      item : state.item , 
-      price : state.price
-    } 
- let finalItems = [...state.items ,newItem] 
+      id: state.id,
+      item: state.item,
+      price: state.price,
+    };
+    let finalItems = [...state.items, newItem];
     setState({
-      items : finalItems , 
-      id : uuidv4(), 
-      item : "",
-      price: ""
-    })
-  } 
+      items: finalItems,
+      id: uuidv4(),
+      item: "",
+      price: "",
+    });
+  };
 
-// console.log(state);
+  let handledelete = (x) => {
+    let filterdata = state.items.filter((value) => value.id !== x);
+    setState({ ...state, items: filterdata });
+  };
 
-let handledelete=(x)=>{
-let filterdata=state.items.filter((value)=>value.id != x)
-setState({...state,items:filterdata})
+  let handleupdate = (y) => {
+    let restitems = state.items.filter((value) => value.id !== y);
+    let datatoedit = state.items.find((value) => value.id === y);
+    setState({
+      ...state,
+      items: restitems,
+      item: datatoedit.item,
+      price: datatoedit.price,
+    });
+  };
 
-}
-
-let handleupdate=(y)=>{
-  let restitems=state.items.filter((value)=>value.id != y)
-  let datatoedit=state.items.find((value)=>value.id==y)
-setState({...state,
-  items:restitems,
-item:datatoedit.item,
-price:datatoedit.price})
-
-}
   return (
     <>
       <article>
         <h1>ToDoList</h1>
-        <main>
+        <main className="main-container">
           <Form
             item={state.item}
             price={state.price}
             handleChange={handleChange}
             handleSubmit={handleSubmit}
           />
-          <TodoList items={state.items} handledelete={handledelete} handleupdate={handleupdate}/>
+          <TodoList
+            items={state.items}
+            handledelete={handledelete}
+            handleupdate={handleupdate}
+          />
         </main>
       </article>
     </>
